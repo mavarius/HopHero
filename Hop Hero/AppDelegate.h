2c0e7b778f58bbcc7adf44b87b8df9a1a2ce051e
Daniel Campbell <metalwhirlwind@hotmail.com>
2016-01-07 18:30:16 -0800

    Initial Commit

---
+++ Hop Hero/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hop Hero
+//
+//  Created by Daniel Campbell on 1/7/16.
+//  Copyright © 2016 Daniel Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hop Hero/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Hop Hero
+//
+
+//  Copyright (c) 2016 Daniel Campbell. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
